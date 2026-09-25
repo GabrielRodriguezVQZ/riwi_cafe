12b3f6ede04a0766afc1dcd451d94d92d46ac6c4
Eduardo Pérez <eduardoperezpl1901@gmail.com>
2026-09-25 17:13:43 -0500

    feat: add table module

--- src/modules/tables/enums/table-zone.enum.ts
+++ src/modules/tables/enums/table-zone.enum.ts
@@ -1,10 +1,4 @@
 export enum TableZone {
-<<<<<<< HEAD
     PLANTA1 = 'PLANTA1',
     PLANTA2 = 'PLANTA2'
 }
-=======
-  PLANTA1 = 'PLANTA1',
-  PLANTA2 = 'PLANTA2'
-}
->>>>>>> 040157e (feat: entities and DTO)
