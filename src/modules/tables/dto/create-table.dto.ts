93691e0509ab7e03ec471e63c831ed7a173874cf
luis <pinamerinoluis@gmail.com>
2026-09-25 15:44:29 -0500

    feat: entities and DTO

--- src/modules/tables/dto/create-table.dto.ts
+++ src/modules/tables/dto/create-table.dto.ts
@@ -1,1 +1,32 @@
-export class CreateTableDto {}
+import { Transform, Type } from 'class-transformer';
+
+import {
+    IsEnum,
+    IsInt,
+    IsNotEmpty,
+    IsString,
+    Max,
+    MaxLength,
+    Min,
+} from 'class-validator';
+
+import { TableZone } from '../enums/table-zone.enum.js';
+
+export class CreateTableDto {
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(50)
+    @Transform(({ value }) =>
+        typeof value === 'string' ? value.trim() : value,
+    )
+    tableNumber: string;
+
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
+    @Max(30)
+    capacity: number;
+
+    @IsEnum(TableZone)
+    zone: TableZone;
+}
--- src/modules/tables/enums/table-status.enum.ts
+++ src/modules/tables/enums/table-status.enum.ts
@@ -2,4 +2,8 @@ export enum TableStatus {
   AVAILABLE = 'AVAILABLE',
   OCCUPIED = 'OCCUPIED',
   OUT_OF_SERVICE = 'OUT_OF_SERVICE',
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 040157e (feat: entities and DTO)
